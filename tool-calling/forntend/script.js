06a980f751af976cba4008a89f1a2e878eafd66d
YogeshMahajanGit <mahajanyogesh443@gmail.com>
2025-09-24 21:24:03 +0530

    UI update

--- tool-calling/forntend/script.js
+++ tool-calling/forntend/script.js
@@ -1,1 +1,32 @@
-console.log("working");
+const input = document.querySelector("#input");
+const chatContainer = document.querySelector("#chat-container");
+const askBtn = document.querySelector("#ask-btn");
+
+input.addEventListener("keyup", handleEnter);
+askBtn.addEventListener("click", handleAsk);
+
+function generate(text) {
+  const message = document.createElement("div");
+  message.className = `my-6 bg-neutral-800 p-3 rounded-xl ml-auto max-w-fit`;
+  message.textContent = text;
+  chatContainer.appendChild(message);
+  input.value = "";
+}
+
+function handleAsk(e) {
+  const text = input.value.trim();
+
+  if (!text) return;
+
+  generate(text);
+}
+
+function handleEnter(e) {
+  if (e.key === "Enter") {
+    const text = input.value.trim();
+
+    if (!text) return;
+
+    generate(text);
+  }
+}
